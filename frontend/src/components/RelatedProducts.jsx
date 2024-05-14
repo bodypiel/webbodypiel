8932ab64ee761ad4a9bc905f8d01e6d21bb0d2de
Oscar Lindo <77596284+oscar2697@users.noreply.github.com>
2024-05-14 00:18:48 -0500

    Arreglos

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -15,8 +15,6 @@ const RelatedProducts = () => {
                             id={item.id} 
                             image={item.image} 
                             name={item.name} 
-                            new_price={item.new_price} 
-                            old_price={item.old_price}
                         />
                     ))}
                 </div>
